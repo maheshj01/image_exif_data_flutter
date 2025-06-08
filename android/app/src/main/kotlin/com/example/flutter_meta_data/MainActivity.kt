e08420f398b82809bf0c71f7e9c2c98a34e4ac3c
mahesh jamdade <maheshmn121@gmail.com>
2025-06-08 00:21:54 -0400

    use camera awesome

--- android/app/src/main/kotlin/com/example/flutter_meta_data/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_meta_data
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/camera.dart
+++ lib/camera.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:camerawesome/camerawesome_plugin.dart';
 import 'package:camerawesome/pigeon.dart';
 import 'package:flutter/material.dart';
@@ -18,6 +20,53 @@ class _CameraPageState extends State<CameraPage> {
       body: Container(
         color: Colors.white,
         child: CameraAwesomeBuilder.awesome(
+          onMediaCaptureEvent: (MediaCapture event) {
+            switch ((event.status, event.isPicture, event.isVideo)) {
+              case (MediaCaptureStatus.capturing, true, false):
+                debugPrint('Capturing picture...');
+              case (MediaCaptureStatus.success, true, false):
+                event.captureRequest.when(
+                  single: (single) {
+                    debugPrint('Picture saved: ${single.file?.path}');
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => ImagePreview(
+                          imageFile: single.file,
+                          onSubmit: () => {
+                            widget.onImageClicked(single.file),
+                            Navigator.of(context).pop(),
+                          },
+                        ),
+                      ),
+                    );
+                  },
+                  multiple: (multiple) {
+                    multiple.fileBySensor.forEach((key, value) {
+                      debugPrint('multiple image taken: $key ${value?.path}');
+                    });
+                  },
+                );
+              case (MediaCaptureStatus.failure, true, false):
+                debugPrint('Failed to capture picture: ${event.exception}');
+              case (MediaCaptureStatus.capturing, false, true):
+                debugPrint('Capturing video...');
+              case (MediaCaptureStatus.success, false, true):
+                event.captureRequest.when(
+                  single: (single) {
+                    debugPrint('Video saved: ${single.file?.path}');
+                  },
+                  multiple: (multiple) {
+                    multiple.fileBySensor.forEach((key, value) {
+                      debugPrint('multiple video taken: $key ${value?.path}');
+                    });
+                  },
+                );
+              case (MediaCaptureStatus.failure, false, true):
+                debugPrint('Failed to capture video: ${event.exception}');
+              default:
+                debugPrint('Unknown event: $event');
+            }
+          },
           saveConfig: SaveConfig.photo(
             exifPreferences: ExifPreferences(
               saveGPSLocation: true,
@@ -34,28 +83,48 @@ class _CameraPageState extends State<CameraPage> {
           // filter: AwesomeFilter.AddictiveRed,
           previewAlignment: Alignment.center,
           previewFit: CameraPreviewFit.contain,
-          onMediaTap: (mediaCapture) {
-            mediaCapture.isPicture
-                ? debugPrint(
-                    'Picture taken: ${mediaCapture.captureRequest.path}')
-                : debugPrint(
-                    'Video recorded: ${mediaCapture.captureRequest.path}');
-            mediaCapture.captureRequest.when(
-              single: (single) {
-                debugPrint('single: ${single.file?.path}');
-                widget.onImageClicked(single.file);
-                Navigator.of(context).pop();
-              },
-              multiple: (multiple) {
-                multiple.fileBySensor.forEach((key, value) {
-                  debugPrint('multiple file taken: $key ${value?.path}');
-                  value?.openRead();
-                });
-              },
-            );
-          },
         ),
       ),
     );
+  }
+}
+
+class ImagePreview extends StatelessWidget {
+  final XFile? imageFile;
+  final Function() onSubmit;
+  const ImagePreview(
+      {super.key, required this.imageFile, required this.onSubmit});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Image Preview'),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.check),
+              onPressed: () {
+                Navigator.of(context).pop();
+                onSubmit();
+                // Call the onSubmit function to handle the image
+                // widget.onImageClicked(imageFile);
+              },
+            ),
+          ],
+        ),
+        body: Center(
+          child: imageFile != null
+              ? Image.file(
+                  File(imageFile!.path),
+                  fit: BoxFit.cover,
+                )
+              : const Text('No image selected'),
+        ));
   }
 }
